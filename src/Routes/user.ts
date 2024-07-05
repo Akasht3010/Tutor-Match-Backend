c73dac223d62d59753a8307cd57c1fa304cd2a90
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-07-05 21:16:37 +0530

    Created the register api

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -7,7 +7,11 @@ import dotenv from 'dotenv'
 import multer from 'multer'
 import path from 'path'
 
+import UserModel from '../../models/user'
+
 const router = express.Router()
+const salt = bcrypt.genSaltSync(10)
+const secret = process.env.SECRET_KEY as string
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -24,8 +28,47 @@ const uploadMiddleware = multer({ storage })
 
 router.use(cookieParser())
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Hello World')
+router.post('/register', uploadMiddleware.single('profilePic'), async (req: Request, res: Response) => {
+  try {
+    const file = req.file
+
+    if (!file) {
+      res.status(400).json({ message: 'Please upload a file' })
+      return
+    }
+
+    const { filename } = file
+    const profilePic = `uploads/${filename}`
+
+    const { firstName, lastName, email, phone, gender, dob, password } = req.body
+    const existingUser = UserModel.findOne({ email })
+
+    if (!existingUser) {
+      res.status(400).json({ message: 'User already exists' })
+      return
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    try {
+      const newUser = await UserModel.create({
+        firstName,
+        lastName,
+        email,
+        phone,
+        gender,
+        dob,
+        profilePic,
+        password: hashedPassword
+      })
+
+      res.status(201).json({ message: 'User created successfully', user: newUser })
+    } catch (err) {
+      console.log(err)
+    }
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 export default router
